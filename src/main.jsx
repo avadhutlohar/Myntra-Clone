4c71ed15aa5845cbe85ad9d0d4a9db579c6b9e11
Avadhut Lohar <loharavdhoot@gmail.com>
2023-12-09 10:28:14 +0530

    Implemented components and added routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./routes/App.jsx";
+import "./index.css";
+import { Router, RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomeItem from "./components/HomeItem.jsx";
+import Bag from "./routes/Bag.jsx";
+import Home from "./routes/Home.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/bag",
+        element: <Bag />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
