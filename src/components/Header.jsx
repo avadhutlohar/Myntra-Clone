c81c55beef708ca2aec20f0d3ea6caa3004c36c5
Avadhut Lohar <loharavdhoot@gmail.com>
2023-12-10 14:21:56 +0530

    Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import { IoPerson } from "react-icons/io5";
 import { FaFaceGrinHearts } from "react-icons/fa6";
 import { BsBagFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 function Header() {
+  const bag = useSelector((store) => store.bag);
   return (
     <header>
       <div className="logo_container">
@@ -47,7 +49,7 @@ function Header() {
         <Link className="action_container" to="/bag">
           <BsBagFill />
           <span className="action_name">Bag</span>
-          <span className="bag-item-count">0</span>
+          <span className="bag-item-count">{bag.length}</span>
         </Link>
       </div>
     </header>
--- src/routes/Bag.jsx
+++ src/routes/Bag.jsx
@@ -1,17 +1,26 @@
-import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import BagItem from "../components/BagItem";
+import BagSummary from "../components/BagSummary";
+import { useSelector } from "react-redux";
 
 const Bag = () => {
+  const bagItems = useSelector((state) => state.bag);
+  const items = useSelector((state) => state.items);
+  const finalItems = items.filter((item) => {
+    const itemIndex = bagItems.indexOf(item.id);
+    return itemIndex >= 0;
+  });
+
   return (
-    <>
-      <main>
-        <div className="bag-page">
-          <div className="bag-items-container"></div>
-          <div className="bag-summary"></div>
+    <main>
+      <div className="bag-page">
+        <div className="bag-items-container">
+          {finalItems.map((item) => (
+            <BagItem item={item} />
+          ))}
         </div>
-      </main>
-    </>
+        <BagSummary />
+      </div>
+    </main>
   );
 };
 
